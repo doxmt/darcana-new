2b2ace36f8b5fe0f60535223130b1680deca0062
songtaeha <songtaeha@naver.com>
2025-12-06 19:24:18 +0900

    feat: 카드 데이터 추가, 랜덤카드뽑기 함수 구현

---
+++ src/util/draw-card.ts
@@ -0,0 +1,19 @@
+import { allCards } from "../data/CardData";
+
+export type DrawResult = {
+  id: number;
+  nameKo: string;
+  isReversed: boolean;
+};
+
+export function drawMajorArcana(): DrawResult {
+  const cardId = Math.floor(Math.random() * 22);
+
+  const card = allCards[cardId];
+
+  return {
+    id: cardId,
+    nameKo: card.nameKo,
+    isReversed: Math.random() < 0.5,
+  };
+}
