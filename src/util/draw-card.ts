37036666ad2a25a0431dd32501fa48d217f0115f
songtaeha <songtaeha@naver.com>
2025-12-10 22:45:34 +0900

    feat: 전체랜덤카드 뽑기함수 구현(메이저+마이너)

--- src/util/draw-card.ts
+++ src/util/draw-card.ts
@@ -17,3 +17,15 @@ export function drawMajorArcana(): DrawResult {
     isReversed: Math.random() < 0.5,
   };
 }
+
+export function drawAllArcana(): DrawResult {
+  const cardId = Math.floor(Math.random() * 76);
+
+  const card = allCards[cardId];
+
+  return {
+    id: cardId,
+    nameKo: card.nameKo,
+    isReversed: Math.random() < 0.5,
+  };
+}
